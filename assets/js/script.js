c054152168c73f472b6c13ad72ac8c397848d72f
BjornRodin <bjornrodin@gmail.com>
2023-03-15 18:00:31 +0000

    feat: Getting the constants in script.js so i can work on them in the script. Also add comments for what sections i might be needing

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+// Getting the constants so we can work on them in the script
+
+const boxes = document.getElementsByClassName('square');
+const mole = document.getElementsByClassName('mole');
+const hits = document.getElementById('hit');
+const misses = document.getElementById('miss');
+const timeLeft = document.getElementById('time');
+
+// RunGame
+
+// CheckIfHit
+
+// IncrementIfHit
+
+// IncrementIfMiss
