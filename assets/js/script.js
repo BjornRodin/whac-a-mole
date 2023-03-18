9336f6fb19340565117928f7e1bc1728c73c8309
BjornRodin <bjornrodin@gmail.com>
2023-03-18 16:19:42 +0000

    feat: Add function randomBox which first remove mole and then choosing a box to put it in

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,11 +9,21 @@ const timeLeft = document.querySelector('#time');
 // Telling javascript that we start at 0 hits
 let result = 0
 
-// RunGame
+/**
+ * First we remove any 'mole' in the grid,
+ * then adding it in by choosing a random box to put the mole in
+ */
 
-function runGame() {
+function randomBox() {
+    boxes.forEach(box => {
+        box.classList.remove('mole')
+    })
 
+    let randomMole = boxes[Math.floor(Math.random() * 9)]
+    console.log(randomMole)
 }
+
+randomBox()
 
 
 // CheckIfHit
