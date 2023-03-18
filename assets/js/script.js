ac7de419f3a9dfedcfad04cfa7bb0fcc23a554cb
BjornRodin <bjornrodin@gmail.com>
2023-03-18 16:59:00 +0000

    feat: Add eventlistener for mouseclick on existing mole and adding it to number of hits. Not working fully yet, working on it.

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,12 +6,16 @@ const hits = document.querySelector('#hit');
 const misses = document.querySelector('#miss');
 const timeLeft = document.querySelector('#time');
 
-// Telling javascript that we start at 0 hits
+/**
+ * Telling Javascript we start at 0 hits
+ * defining hitmole element
+ */
 let result = 0
-
+let hitMole
 /**
  * First we remove any 'mole' in the grid,
- * then adding it in by choosing a random box to put the mole in
+ * then adding it in by choosing a random box to put the mole in,
+ * then checking if we hit the random box the mole appear in
  */
 
 function randomBox() {
@@ -21,7 +25,19 @@ function randomBox() {
 
     let randomMole = boxes[Math.floor(Math.random() * 9)]
     randomMole.classList.add('mole')
+
+    hitMole = randomBox.id
 }
+
+boxes.forEach(box => {
+    box.addEventListener('mousedown', () => {
+        if (box.id == hitMole) {
+            result++
+            hits.textContent = result
+            hitMole = null
+        }
+    })
+})
 
 randomBox()
 
